a652a2da0c8b6019c277e8975a3bfa9ebe849daa
Bjarki Þór Elvarsson <bthe@hafro.is>
2016-01-06 15:10:30 +0000

    - First attempt for a trial specifications template

---
+++ trials/trials.R
@@ -0,0 +1,117 @@
+library(dplyr)
+library(infuser)
+
+variables_requested('trials/trial_template.txt')
+
+
+
+trial <- function(hypo=1,msyr=0.01,trialtype='B'){
+  
+  ## defaults 
+  nstk <- 6
+  nsuba <- 7
+  ngamma <- 6
+  ndelta <- 2
+  
+  ## deviations  
+  if(hypo %in% c(6,7,8)){
+    nstk <- 5
+  }
+  
+  if(hypo %in% c(7,8)){
+    nsuba <- 6
+  }
+  
+  if(hypo %in% c(4,8)){
+    ndelta <- 0
+  }
+  
+  ## mixing matricies
+  if(hypo==1){ 
+    mixmat <- 
+"1    1          1   1   0   0    0   0   0    1  4  0  0  0  0  0
+2    1          0   0   1   0    0   0   0    0  0  0  0  0  0  0
+3    1          0   0   0   1    0   0   0    0  0  0  0  0  0  0
+4    1          0   0   0   0    1   0   0    0  0  0  0  0  0  0
+5    1          0   0   0   0    0   1   0    0  0  0  0  0  0  0
+6    1          0   0   0   0    0   0   1    0  0  0  0  0  0  0"
+    
+  } else if(hypo==2) {
+    mixmat <-
+"1    1         .88 .88 .1  .02   0   0   0    1  4  0  0  0  0  0 
+2    1          0   0   1   0    0   0   0    0  0  0  0  0  0  0
+3    1          0   0   0   1    0   0   0    0  0  0  0  0  0  0
+4    1          0   0   0   0    1   0   0    0  0  0  0  0  0  0
+5    1          0   0   0  .02  .1  .88  0    0  0  0  0  0  0  0
+6    1          0   0   0   0    0   0   1    0  0  0  0  0  0  0"
+  } else if(hypo==3) {
+    mixmat <- 
+"1    1          1   1   0   0   0   0   0     1  4  0  0  0  0  0
+2    1         .1  .1  .9   0   0   0   0     1  4  0  0  0  0  0
+3    1          0   0   0   1   0   0   0     0  0  0  0  0  0  0
+4    1          0   0   0   0  .9  .1   0     0  0  0  0  0  0  0
+5    1          0   0   0   0   0   1   0     0  0  0  0  0  0  0
+6    1          0   0   0   0   0   0   1     0  0  0  0  0  0  0"
+  } else if(hypo == 4){
+    mixmat <- 
+"1    1          1   1   0   0   0   0   0     1  4  0  0  0  0  0
+2    1          1   1   1   1   0   0   0     2  6  5  3  0  0  0
+3    1          0   0   1   1   1   0   0     0  0  3  5  3  0  0
+4    1          0   0   0   1   1   1   0     0  0  0  3  5  3  0
+5    1          0   0   0   0   0   1   0     0  0  0  0  0  0  0
+6    1          0   0   0   0   0   0   1     0  0  0  0  0  0  0"
+  } else if(hypo == 5){
+    mixmat <- 
+"1    1          1   1   0   0   0   0   0     1  4  0  0  0  0  0
+2    1          0   0   1   0   0   0   0     0  0  0  0  0  0  0
+3    1          0   0   0   1   0   0   0     0  0  0  0  0  0  0
+4    1          0   0   0   0   1   0   0     0  0  0  0  0  0  0
+5    1          0   0   0   0   0   1   0     0  0  0  0  0  0  0
+6    1          0   0   0   0  .02 .1  .88    0  0  0  0  0  0  0"
+  } else if(hypo == 6){
+    mixmat <- 
+"1    1         .88 .88 .1  .02   0   0   0    1  4  0  0  0  0  0
+2    1          0   0   1   0    0   0   0    0  0  0  0  0  0  0
+3    1          0   0   0   1    0   0   0    0  0  0  0  0  0  0
+4    1          0   0   0   0    1   1   0    0  0  0  0  3  2  0
+5    1          0   0   0   0    0   0   1    0  0  0  0  0  0  0"
+  } else if(hypo == 7){
+    mixmat <- 
+"1    1          1   1   0   0    0   0        1  4  0  0  0  0
+2    1          1   1   1   0    0   0        5  6  3  0  0  0
+3    1          0   0   0  .9   .1   0        0  0  0  0  0  0
+4    1          0   0   0   0    1   0        0  0  0  0  0  0
+5    1          0   0   0   0    0   1        0  0  0  0  0  0"
+  } else if(hypo == 8){
+    mixmat <- 
+"1    1          1   1   0   0    0   0        1  4  0  0  0  0 
+2    1          1   1   1   1    0   0        2  6  5  3  0  0 
+3    1          0   0   1   1    1   0        0  0  3  5  3  0 
+4    1          0   0   0   0    1   0        0  0  0  0  0  0 
+5    1          0   0   0   0    0   1        0  0  0  0  0  0"
+  }
+  
+  ## trial code generated
+  ref <- 'NF-{{trialtype}}{{hypo}}-{{msyr}} H{{hypo}}-{{msyr}}' %>%
+    infuse(hypo=hypo,msyr=100*msyr,trialtype=trialtype)
+  
+  ## assign msyr rates to nstk stocks
+  msyr.txt <- 
+    sprintf('MSYR OF STOCK 1 (MSYR%1$s(%1$s))                   %2$s',
+            1:nstk,msyr) %>%
+    paste(collapse='\n')
+  
+  ## change the density dependance from 1+ for 1% to mature for 4%
+  if(msyr==0.01){
+    optf <- 1
+  } else {
+    optf <- 0
+  }
+  
+  ## produce the trial setup
+  infuse('trials/trial_template.txt',
+         list(ref=ref,
+              msyrtxt=msyr.txt,
+              optf=optf,nstk=nstk,
+              mixmat))
+}
