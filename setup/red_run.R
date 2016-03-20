ca23afa270d7b173a3d8c14a9e731328b5a8fb26
bthe <bjarki.elvarsson@gmail.com>
2016-03-20 15:18:35 +0000

    Runs defined

--- setup/red_run.R
+++ setup/red_run.R
@@ -6,17 +6,15 @@ source('setup/read_results.R')
 
 NafSetup('RedAbund')
 NafCond(dir='RedAbund',survey='../data/survey-red.dat',
-        search.string = 'NF-.[0-6]-..dat')
-NafCond(dir='RedAbund',survey='../data/survey-red7.dat',
-        search.string = 'NF-.[7|8]-..dat')
+        search.string = 'NF-B[1|2|3|5|6]-..dat')
 NafResults('RedAbund',db_name='RedAbund.db')
 NafVariants(dir='RedAbund',var.search = 'Naf-v[0-2].par',
-            search.string = 'NF-.[0-6]-..dat',
+            search.string = 'NF-B[1|2|3|5|6]-..dat',
             survey='../data/survey-red.dat')
 file.copy('settings/clc-n-rev.par',to = 'RedAbund/clc-n.par',overwrite = TRUE)
 NafVariants(dir='RedAbund',var.search = 'Naf-v[3-7].par',
-            search.string = 'NF-.[7-8]-..dat',
-            survey='../data/survey-red7.dat')
+            search.string = 'NF-B[1|2|3|5|6]-..dat',
+            survey='../data/survey-red.dat')
 ManSetup(dir='outn-Red',db_name ='RedAbund.db')
 ManRun(dir='outn-Red')
 NafReadVariants(dir='RedAbund',search.string='NF-..-.-V[0-9].restest',db_name='RedAbund_res.db')
